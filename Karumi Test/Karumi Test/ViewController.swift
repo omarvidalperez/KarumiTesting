8e0fff4f5b603de92053584356c59e94aa17d353
Omar Vidal Pérez <o.vidal.perez@mobgen.com>
2019-11-12 10:30:12 +0100

    create project

---
+++ Karumi Test/Karumi Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Karumi Test
+//
+//  Created by Omar Vidal Pérez on 12/11/2019.
+//  Copyright © 2019 Omar Vidal Pérez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
